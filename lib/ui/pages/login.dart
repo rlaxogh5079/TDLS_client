894160ee4558cd4a3d65ecf8847cdaf6d61f54a2
rlaxogh5079 <rlaxogh507906@gmail.com>
2024-09-14 22:43:23 +0900

    :sparkles: 메인 화면 기능 구현

---
+++ lib/ui/pages/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BLSLoginPage extends StatefulWidget {
+  const BLSLoginPage({super.key});
+
+  @override
+  State<BLSLoginPage> createState() => _BLSLoginPageState();
+}
+
+class _BLSLoginPageState extends State<BLSLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello World! This is BLS"),
+    );
+  }
+}
